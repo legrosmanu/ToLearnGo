0a918f9baf304c54b9a5a413aa6148c9f0433330
Emmanuel Legros <e_legros@icloud.com>
2016-12-19 09:08:29 +0100

    Add comments

--- src/simple/hello.go
+++ src/simple/hello.go
@@ -1,5 +1,9 @@
 package main
 
+/*********************************************************************************
+* Just a hello world, with a try of the function which have 2 variables on return.
+**********************************************************************************/
+
 import (
 	"fmt"
 	"strconv"
@@ -7,12 +11,12 @@ import (
 
 func multipleVarReturn(first int, second float64) (label string, result string) {
 	label = "The paramaters are : "
-	result = strconv.Itoa(first) + " " + strconv.FormatFloat(second, 'E', -1, 64)
+	result = strconv.Itoa(first) + " and " + strconv.FormatFloat(second, 'E', -1, 64)
 	return
 }
 
 func main() {
 	fmt.Println("Hello, GO")
 	firstReturn, secondeReturn := multipleVarReturn(10, 3.14)
-	fmt.Println(firstReturn + secondeReturn)
+	fmt.Println(firstReturn, secondeReturn)
 }
--- src/staticWebSite/webServer.go
+++ src/staticWebSite/webServer.go
@@ -7,6 +7,7 @@ import (
 
 // "go run" must be launched on the webServer.go directory. It's to find the "gui" directory.
 func main() {
+	// The static files are on the gui directory.
 	fs := http.FileServer(http.Dir("gui"))
 	http.Handle("/", fs)
 
